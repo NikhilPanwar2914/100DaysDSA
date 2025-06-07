c13b0086d4ab5174bff8803a7a3a8b52e6feab27
NikhilPanwar2914 <nikhil.panwar1414@gmail.com>
2025-06-07 10:47:22 +0530

    Initial commit

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+function reverseString(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+  return arr;
+}
+console.log(reverseString(["h", "e", "l", "l", "o"]));
